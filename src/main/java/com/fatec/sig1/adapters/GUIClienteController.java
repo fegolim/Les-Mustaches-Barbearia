b64da7b73b9c9f9dea2c786bc95257d8566a9a52
Fernanda <fernanda.lima4@fatec.sp.gov.br>
2022-11-29 17:24:53 -0300

    ajustes roles x acessos

--- src/main/java/com/fatec/sig1/adapters/GUIClienteController.java
+++ src/main/java/com/fatec/sig1/adapters/GUIClienteController.java
@@ -6,7 +6,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PostAuthorize;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/fatec/sig1/security/WebSecurityConfig.java
+++ src/main/java/com/fatec/sig1/security/WebSecurityConfig.java
@@ -16,21 +16,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         //http.authorizeRequests().antMatchers("/").hasAnyRole("ADMIN", "VEND", "OUTROS") //
         http.csrf().disable()
-		.authorizeRequests().antMatchers("/").permitAll() //
-                .antMatchers("/profissional").hasRole("ADMIN") // somente login maria
+		.authorizeRequests().antMatchers("/").permitAll()
+                .antMatchers("/profissional").hasRole("ADMIN")
                 .antMatchers("/sig/cliente", "/sig/cliente/**").hasAnyRole("ADMIN", "VEND")
 				.antMatchers("/sig/clientes", "/sig/clientes/**").hasAnyRole("ADMIN", "VEND")
                 .antMatchers("/sig/profissional", "/sig/profissional/**").hasAnyRole("ADMIN", "OUTROS")
-				.antMatchers("/sig/profissionais", "/sig/profissionais/**").hasAnyRole("ADMIN", "OUTROS")
-				//.antMatchers("/sig/pedidos", "/sig/pedidos/**").hasRole("OUTROS")
-				//.antMatchers("/sig/cliente", "/sig/cliente/**").hasRole("OUTROS")
-				//.antMatchers("/sig/clientes", "/sig/clientes/**").hasRole("OUTROS")
-
+				.antMatchers("/sig/profissionais", "/sig/profissionais/**").hasRole("ADMIN")
                 .antMatchers("/sig/pedido", "/sig/pedido/**").hasAnyRole("ADMIN", "OUTROS")
 				.antMatchers("/sig/pedidos", "/sig/pedidos/**").hasAnyRole("ADMIN", "OUTROS")
-                .antMatchers("/sig/produto", "/sig/produto/**").hasAnyRole("ADMIN", "VEND")
+                .antMatchers("/sig/produto", "/sig/produto/**").hasAnyRole("ADMIN", "VEND", "OUTROS")
 				.antMatchers("/sig/produtos", "/sig/produtos/**").hasAnyRole("ADMIN", "VEND")
-                .antMatchers("/sig/servico", "/sig/servico/**").hasAnyRole("ADMIN", "OUTROS")
+                .antMatchers("/sig/servico", "/sig/servico/**").hasAnyRole("ADMIN", "VEND", "OUTROS")
 				.antMatchers("/sig/servicos", "/sig/servicos/**").hasAnyRole("ADMIN", "OUTROS")
                 .anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
                 .logoutSuccessUrl("/login?logout").permitAll().and()
